e9b6284084b02ad9ccbc865f585b86b68f53958a
Jonathan <jgibler@gmail.com>
2025-07-15 13:48:27 +0000

    initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5027', // Replace with your backend port
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
+
